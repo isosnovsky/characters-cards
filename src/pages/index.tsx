1728d7018ff3ee1fbb9332be29e87ff7d6fd9302
isosnovski <ilya.sosnovskiy@leroymerlin.ru>
2023-05-06 20:48:03 +0300

    fix: add correct routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 
 import { Character } from '@/pages/character'
 import { Main } from '@/pages/main'
+import { NotFound } from '@/pages/not-found'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
-    errorElement: <div>error</div>,
-    children: [
-      {
-        path: '/character/:characterId',
-        element: <Character />,
-      },
-    ],
+    errorElement: <NotFound />,
+  },
+  {
+    path: '/character/:characterId',
+    element: <Character />,
   },
 ])
