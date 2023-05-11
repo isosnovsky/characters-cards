726269a9d55637782f6791312a3ba66ac2b03366
isosnovski <ilya.sosnovskiy@leroymerlin.ru>
2023-05-11 05:13:02 +0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  base: 'characters-card',
+  base: '/characters-cards/',
 })
